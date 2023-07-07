9344eb6f200ba9b6dd213ed3c907610e54bd0ad7
양희연 <deepbm27@gmail.com>
2023-07-07 15:02:40 +0900

    Add: update todo status

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -7,11 +7,15 @@ export default function ToDoList() {
   const handleAdd = todo => {
     setTodos(prev => [...prev, todo]);
   };
+  const handleUpdate = updated => {
+    setTodos(prev => prev.map(todo => (todo.id === updated.id ? updated : todo)));
+  };
+
   return (
     <>
       <ul>
         {todos.map(todo => (
-          <ToDoListItem key={todo.id} todo={todo} />
+          <ToDoListItem key={todo.id} todo={todo} onUpdate={handleUpdate} />
         ))}
       </ul>
       <AddForm onAdd={handleAdd} />
--- src/components/ToDoListItem.jsx
+++ src/components/ToDoListItem.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { BsFillTrashFill } from 'react-icons/bs';
 
-export default function ToDoListItem({ todo: { id, text, status } }) {
+export default function ToDoListItem({ todo, todo: { id, text, status }, onUpdate }) {
+  const handleChange = e => {
+    const updated = { ...todo, status: e.target.checked ? 'completed' : 'active' };
+    onUpdate(updated);
+  };
+
   return (
     <li>
-      <input type='checkbox' id={id} name='text' />
+      <input type='checkbox' id={id} name='text' onChange={handleChange} />
       <label for={id}>{text}</label>
       <button>
         <BsFillTrashFill />
