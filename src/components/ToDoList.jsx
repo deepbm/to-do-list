9469402980160f3c95460348ce0317d1c9bb628e
양희연 <deepbm27@gmail.com>
2023-07-07 16:57:19 +0900

    Add: save to localStorage

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -3,7 +3,7 @@ import ToDoListItem from './ToDoListItem';
 import AddForm from './AddForm';
 
 export default function ToDoList() {
-  const [todos, setTodos] = useState(initialTodos);
+  const [todos, setTodos] = useState(readTodosFromLocalstorage);
   const [total, setTotal] = useState(0);
   const handleAdd = todo => {
     setTodos(prev => [...prev, todo]);
@@ -17,22 +17,29 @@ export default function ToDoList() {
 
   useEffect(() => {
     setTotal(todos.length);
+    localStorage.setItem('todos', JSON.stringify(todos));
   }, [todos]);
 
   return (
     <>
       <p>{total}</p>
       <ul>
-        {todos.map(todo => (
-          <ToDoListItem key={todo.id} todo={todo} onUpdate={handleUpdate} onDelete={handleDelete} />
-        ))}
+        {todos.length > 0 &&
+          todos.map(todo => (
+            <ToDoListItem
+              key={todo.id}
+              todo={todo}
+              onUpdate={handleUpdate}
+              onDelete={handleDelete}
+            />
+          ))}
       </ul>
       <AddForm onAdd={handleAdd} />
     </>
   );
 }
 
-const initialTodos = [
-  { id: 1, text: 'Meeting', status: 'active' },
-  { id: 2, text: 'Pay bills', status: 'active' },
-];
+const readTodosFromLocalstorage = () => {
+  const todos = localStorage.getItem('todos');
+  return todos ? JSON.parse(todos) : [];
+};
